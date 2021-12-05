b8af1e89cd0bd7f1cd508d19737dfdae485f0425
Yajana N Rao <yajananrao@gmail.com>
2021-12-05 22:03:24 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <DataFlow />
+  </div>
+</template>
+
+<script>
+import DataFlow from "./components/DataFlow";
+
+export default {
+  name: "App",
+  components: {
+    DataFlow,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
